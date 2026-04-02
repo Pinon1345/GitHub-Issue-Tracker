9de5b87d2289e4b1b0898e05d15ed752dbd485b0
Pinon1345 <pinonfurkan1@gmail.com>
2026-04-02 13:34:17 +0600

    added dashboard file after signin

--- script/script.js
+++ script/script.js
@@ -1,2 +1,15 @@
 // console.log("Hello world");
 
+function signIn () {
+    const username = document.getElementById ("username").value;
+    const password = document.getElementById ("password").value;
+
+    if (username === "admin" & password === "admin123") {
+        alert ("Congratulation! Your Sign In is Successful.");
+        window.location.href ="../dashboard.html"
+        
+    }
+    else{
+        alert ("Invalid Sign In, Please try again.")
+    }
+}
